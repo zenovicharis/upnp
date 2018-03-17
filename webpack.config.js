a160bd9d83440984917a5dfeeafa1e5ab613f835
hexadecimalH <zenovicharis@gmail.com>
2018-03-17 12:27:52 +0100

    Added jquery to configuration, and implemented header

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -60,7 +61,9 @@ module.exports = {
                 ]
             },
             { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
-            { test: /\.(ttf|eot)$/, loader: 'file-loader' }
+            { test: /\.(ttf|eot)$/, loader: 'file-loader' },
+            // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
     },
     devServer:{
@@ -77,6 +80,24 @@ module.exports = {
             template: 'src/index.html',
             allChunks:false
         }),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(['dist']),
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery",
+          Popper: 'popper.js/dist/umd/popper.js',
+          Tether: 'tether',
+          'window.Tether': 'tether',
+          Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+          Button: 'exports-loader?Button!bootstrap/js/dist/button',
+          Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+          Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+          Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+          Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+          Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+          Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+          Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+          Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+          Util: 'exports-loader?Util!bootstrap/js/dist/util'
+        })
     ]
 };
